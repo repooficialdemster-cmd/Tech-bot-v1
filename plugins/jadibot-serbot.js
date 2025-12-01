28603817fffbe4a85a0b7a90230f28972e33e181
repooficialdemster-cmd <repooficialdemster@gmail.com>
2025-11-30 21:25:46 -0400

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -99,7 +99,7 @@ global.db.data.users[m.sender].Subs = new Date * 1
 
 handler.help = ['qr', 'code']
 handler.tags = ['other']
-handler.command = ['code 54', 'qr']
+handler.command = ['code', 'qr']
 export default handler 
 
 export async function yukiJadiBot(options) {
